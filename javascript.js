f5a9f77de2b94c4461fbf86390e16e1fd1f19dc7
smmasudrana <smmasudrana62@gmail.com>
2025-07-29 06:47:02 +0600

    add conditional intro

---
+++ javascript.js
@@ -0,0 +1,20 @@
+/**
+ * comparison
+ * 1. bigger: >
+ * 2. less : <
+ * 3. equal: ==
+ * 4. grater than or equal: >=
+ * 5. less than or equal: <=
+ * 6. not equal:!=
+ * 7. and:&&
+ * 8. or: ||
+**/ 
+
+console.log(5>10)
+console.log(5<10)
+
+console.log(10 == '10')
+console.log(10 === '10')
+
+console.log(10 !== '10')
+console.log(10 !== '10')
